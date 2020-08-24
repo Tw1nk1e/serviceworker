728980951d7b3a91f7d6eabdbfd74bcac170cc67
Aleksandr <33854738+Tw1nk1e@users.noreply.github.com>
2020-08-24 19:55:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-notification.onclick = function(event) {
-  event.preventDefault(); // prevent the browser from focusing the Notification's tab
-  console.log(event, 34343)
-}
+self.onnotificationclick = function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+};
 
 firebase.initializeApp({
     messagingSenderId: '903136559212'
